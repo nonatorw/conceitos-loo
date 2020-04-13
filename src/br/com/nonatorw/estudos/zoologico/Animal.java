4124c3151c33c131f355385b5b386fd4aedeb1fa
NONATO, W. Rodrigo <wellingtonrodrigo.nonato@gmail.com>
2020-04-13 04:48:34 -0300

    Encapsulamento

---
+++ src/br/com/nonatorw/estudos/zoologico/Animal.java
@@ -0,0 +1,34 @@
+package br.com.nonatorw.estudos.zoologico;
+
+public class Animal {
+    /**
+     * Aplicando conceitos de encapsulamento: variaveis privadas e métodos acessores
+     */
+    private String nome;
+    private String especie;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
