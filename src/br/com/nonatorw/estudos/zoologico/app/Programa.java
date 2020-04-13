d4b88343c4f6ee8194d5d03249b230bf0fd8454d
NONATO, W. Rodrigo <wellingtonrodrigo.nonato@gmail.com>
2020-04-13 05:11:34 -0300

    Orientação a Objetos básica

---
+++ src/br/com/nonatorw/estudos/zoologico/app/Programa.java
@@ -0,0 +1,22 @@
+package br.com.nonatorw.estudos.zoologico.app;
+
+import br.com.nonatorw.estudos.zoologico.Animal;
+
+public class Programa {
+    public static void main(String[] args) {
+        // Cachorro é uma Instância da classe "Animal".
+        Animal cachorro = new Animal();
+        cachorro.setNome("Totó");
+        cachorro.setIdade(2);
+        cachorro.setEspecie("cachorro");
+
+        System.out.println("Olá, seu animal é um " + cachorro.getEspecie() +
+                           ", e seu nome é " + cachorro.getNome() +
+                           ". Ele tem " + cachorro.getIdade());
+        if (cachorro.ehAdulto()) {
+            System.out.println("Animal adulto");
+        } else {
+            System.out.println("Animal filhote");
+        }
+    }
+}
